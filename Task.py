c3471cbfcaa56c3c88a2ba1456b0721797d5c200
swetha16-7 <22891a05h0.swetha@gmail.com>
2025-12-12 20:30:57 +0530

    Add files via upload

---
+++ Task.py
@@ -0,0 +1,82 @@
+# 1
+print('Hello SkillGapAI')
+
+# 2
+name = 'Your Name'
+print(name)
+
+# 3
+age = 25
+city = 'Chennai'
+skill = 'Python'
+print(age, city, skill)
+
+# 4
+value = str(100)
+print(type(value))
+
+# 5
+text = 'python basics'
+print(text.upper())
+
+# 6
+print(len('SkillGapAI'))
+
+# 7
+colors = ['red', 'blue', 'green']
+print(colors[1])
+
+# 8
+fruits = ['apple', 'banana']
+fruits.append('orange')
+print(fruits)
+
+# 9
+items = ['apple', 'banana', 'orange']
+items.remove('banana')
+print(items)
+
+# 10
+nums = [1, 2, 3]
+t = tuple(nums)
+print(t)
+
+# 11
+resume = {'name': 'John', 'age': 25, 'skills': ['Python', 'SQL']}
+
+# 12
+print(resume['skills'])  
+
+# 13
+marks = 50
+print('Pass' if marks > 40 else 'Fail')
+
+# 14
+num = 7
+print('Even' if num % 2 == 0 else 'Odd')
+
+# 15
+for i in range(1, 11): 
+    print(i)
+
+# 16
+i = 5
+while i >= 1: 
+    print(i)
+    i -= 1
+
+# 17
+def say_hello(): 
+    print('Hello')
+say_hello()
+
+# 18.
+def add(a, b): 
+    return a + b
+print(add(2,3))
+
+#19
+import math
+print(math.pi)
+
+
