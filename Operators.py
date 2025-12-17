f5fe080a18a51e5ec70b0df79c74ca8ad1b3a14c
swetha16-7 <22891a05h0.swetha@gmail.com>
2025-12-17 18:20:35 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,22 @@
+a = 15
+b = 4
+c = 9
+d = 20
+
+num= [4, 9, 15, 25]
+
+print(a + b)          # Output: 19
+print(d - c)          # Output: 11
+print(b * 2)          # Output: 8
+print(a % b)          # Output: 3
+print(c > b)          # Output: True
+print((a * b) - c)    # Output: 51
+print(d // b)         # Output: 5
+print((a > c) and (b < d))   # Output: True
+print(a in num)      # Output: True
+print(c ** 2)         # Output: 81
+print(((a + c) // b) + (d % a))  # Output: 11
+print((a ** b) // c)  # Output: 5625
+print((a < d) or (c == b))  # Output: True
+print(num is num)   # Output: True
+print((d // b) ** 2 - (a % c))  # Output: 19
