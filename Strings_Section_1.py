4da0d4416a8e81786bc1707775ff46f0af8ef35a
swetha16-7 <22891a05h0.swetha@gmail.com>
2025-12-17 18:25:44 +0530

    Add files via upload

---
+++ Strings_Section_1.py
@@ -0,0 +1,29 @@
+text = "Python"
+print(text[:3])          # Output: Pyt
+
+name = "hello WORLD"
+print(name.lower())      # Output: hello world
+
+msg = " Welcome to Python "
+print(msg.strip())       # Output: Welcome to Python
+
+word = "Programming"
+print(len(word))         # Output: 11
+
+text = "apple,banana,grape"
+print(text.split(","))   # Output: ['apple', 'banana', 'grape']
+
+text = "I love Java"
+print(text.replace("Java", "Python"))  # Output: I love Python
+
+line = "Data Science"
+print(line[5:])          # Output: Science
+
+text = "Python"
+print(text[-1])          # Output: n
+
+sentence = "Learning Python is fun"
+print("Python" in sentence)  # Output: True
+
+words = ["Python", "is", "easy"]
+print(" ".join(words))   # Output: Python is easy
